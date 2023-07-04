c2a2452d8575067edd321d65c9200de87457e310
tomssojer <tom.sojer2@gmail.com>
2023-07-05 00:25:21 +0200

    Updated view on post and delete

--- ui/src/app/modals/delete/delete.component.ts
+++ ui/src/app/modals/delete/delete.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable, forkJoin } from 'rxjs';
+import { TextService } from 'src/app/text.service';
 
 @Component({
   selector: 'app-delete',
@@ -7,11 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['../modals.css'],
 })
 export class DeleteComponent {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private textService: TextService,
+    @Inject(MAT_DIALOG_DATA) public data: { id: number }
+  ) {}
 
-  deleteComponent(endpoint: string) {
+  deleteComponent() {
+    // for (let i = 0; i < idArr.length; i++) {
+    //   console.log('http://localhost:8080/api/' + idArr[i]);
+
     return this.http
-      .delete('http://localhost:8080/api/' + endpoint)
+      .delete('http://localhost:8080/api/text/' + this.data.id)
       .subscribe((res) => {
         console.log(res);
       });
--- ui/src/app/text/text.component.ts
+++ ui/src/app/text/text.component.ts
@@ -4,7 +4,6 @@ import { IText } from '../models/Text';
 import { MatDialog } from '@angular/material/dialog';
 import { PostTextComponent } from '../modals/post-text/post-text.component';
 import { DeleteComponent } from '../modals/delete/delete.component';
-import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-text',
@@ -13,7 +12,9 @@ import { trigger } from '@angular/animations';
 })
 export class TextComponent implements OnInit {
   public textArr: IText[] = [];
+  public checkedArr: number[] = [];
   public itemCount: number = 0;
+  public currentItemId: number | undefined;
 
   constructor(private textService: TextService, public dialog: MatDialog) {}
 
@@ -26,14 +27,20 @@ export class TextComponent implements OnInit {
 
     modalRef.afterClosed().subscribe((res) => {
       console.log('Result: ', res);
+      this.textService.getText().subscribe((data) => (this.textArr = data));
     });
   }
 
   openDeleteModal() {
-    const modalRef = this.dialog.open(DeleteComponent);
+    const modalRef = this.dialog.open(DeleteComponent, {
+      data: { id: this.currentItemId },
+    });
+
+    console.log('current item id: ', this.currentItemId);
 
     modalRef.afterClosed().subscribe((res) => {
       console.log('Result: ', res);
+      this.textService.getText().subscribe((data) => (this.textArr = data));
     });
   }
 
@@ -47,11 +54,24 @@ export class TextComponent implements OnInit {
     ) as HTMLInputElement | null;
 
     if (input != null && button != null) {
+      // If checked is false
       if (input.checked == false) {
         this.itemCount++;
         button.disabled = false;
-      } else {
+        this.checkedArr.push(id);
+
+        // For now we use just one element
+        this.currentItemId = id;
+      }
+      // Else if checked is true
+      else {
         this.itemCount--;
+
+        for (let i = 0; i < this.checkedArr.length; i++) {
+          if (this.checkedArr[i] == id) {
+            this.checkedArr.splice(i, 1);
+          }
+        }
 
         if (this.itemCount == 0) {
           button.disabled = true;
